90781f3daa6a3070ddeaabe8839eef38dc90ce1b
ncwphilly <wnoah@sas.upenn.edu>
2018-02-20 23:19:53 -0500

    fixed stuff

--- practical-pytorch/char-rnn-generation/perplexity.py
+++ practical-pytorch/char-rnn-generation/perplexity.py
@@ -2,46 +2,26 @@ import argparse
 import torch
 from helpers import *
 from model import *
-<<<<<<< HEAD
 import numpy as np
-=======
->>>>>>> master
 
 def evaluate(line_tensor):
     hidden = decoder.init_hidden()
     for i in range(line_tensor.size()[0]):
-<<<<<<< HEAD
         output, hidden = decoder(line_tensor[i], hidden)
-=======
-        output, hidden = rnn(line_tensor[i], hidden)
->>>>>>> master
 
     return output
 
 def perplexity(decoder, test_filename):
-<<<<<<< HEAD
-    softmax = torch.nn.LogSoftmax(dim=1)
-=======
-
->>>>>>> master
     """
-
     :param decoder:
     :param test_filename:
     :type decoder: torch.nn.Module
     :return:
     """
+    softmax = torch.nn.LogSoftmax(dim=1)
     test = open(test_filename, encoding='iso-8859-1').read()
-<<<<<<< HEAD
-=======
-    char_tensor(
->>>>>>> master
-    #pad = "~" * order
-    #test = pad + test
-
     sz = len(test)
     log_sum = 0
-<<<<<<< HEAD
 
     curr_char = test[0]
     for i in range(sz - 1):
@@ -54,30 +34,21 @@ def perplexity(decoder, test_filename):
         prob = output[index_next_char]
 
         """next_letter_prob = model["<UNK>"]
-=======
     curr_char = test[0]
     for i in range(sz):
         output = evaluate()
         next_letter = test[i + order]
         next_letter_prob = model["<UNK>"]
->>>>>>> master
         if phrase in model:
             phrase_probs = model[phrase]
             for j in range(len(phrase_probs)):
                 tup = phrase_probs[j]
                 if tup[0] == next_letter:
                     next_letter_prob = tup[1]
-<<<<<<< HEAD
                     break"""
         log_sum += np.log(prob)
         curr_char = test[i + 1]
-=======
-                    break
-        log_sum += np.log(next_letter_prob)
->>>>>>> master
     return log_sum * (-1 / float(sz))
-
-
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
